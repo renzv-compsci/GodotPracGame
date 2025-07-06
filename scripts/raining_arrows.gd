ed78910b38b375c084a33731d13c3e66d5d0161c
renzv-compsci <viloriarenz917@gmail.com>
2025-07-07 00:48:08 +0800

    Add collision scripts for the arrow

--- scripts/raining_arrows.gd
+++ scripts/raining_arrows.gd
@@ -1,6 +1,19 @@
-extends Node2D
+extends Area2D
+# Moves arrow downwards a a constant speed 
+@export var  speed = 900
 
-const SPEED = 300.0
+func _ready(): 
+    connect("body_entered", _on_body_entered)
+    add_to_group("raining_arrows")
+    z_index = 10
 
-func _process(delta):
-    position.y += SPEED * delta 
+func _on_body_entered(body):
+    print("Colleded with: ", body.name)
+    if body.name == "Chicken": 
+        print("Game Over!")
+        get_tree().paused = true # Pause the game 
+
+func _physics_process(delta):
+    position.y += speed * delta 
+    if position.y > 900: 
+        queue_free()
