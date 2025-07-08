c65ca2a19966eb23f5df0bfc1ce9b1909d425c9e
renzv-compsci <viloriarenz917@gmail.com>
2025-07-08 22:20:34 +0800

    Fix the restart button to direct to the main screen

--- scripts/game_world.gd
+++ scripts/game_world.gd
@@ -6,9 +6,11 @@ extends Node2D
 var window_width = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$RestartButton.visible = false 
 	window_width = get_viewport().get_visible_rect().size.x
 	timer.timeout.connect(_on_timer_timeout)
 	timer.start()
+
 	if scene_to_instance:
 		var instance = scene_to_instance.instantiate()
 		instance.position = Vector2(randf_range(0, window_width), 0)
@@ -26,3 +28,11 @@ func _on_timer_timeout():
 			add_child(spawnedarrows)
 			timer.wait_time = randf_range(0.07, 0.25) #Random internval of arrows 
 			timer.start()
+
+func game_over():
+	get_tree().paused = true
+	$RestartButton.visible = true # Makes the button only visible when the game is over 
+
+func _on_restart_button_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
