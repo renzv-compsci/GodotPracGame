79b8799c8b230884bde8b4051ef66f8883e57b1c
renzv-compsci <viloriarenz917@gmail.com>
2025-07-08 22:20:07 +0800

    Fix the collision between character and arrows

--- scripts/raining_arrows.gd
+++ scripts/raining_arrows.gd
@@ -3,17 +3,19 @@ extends Area2D
 @export var  speed = 900
 
 func _ready(): 
-    connect("body_entered", _on_body_entered)
-    add_to_group("raining_arrows")
-    z_index = 10
+	connect("body_entered", _on_body_entered)
+	add_to_group("raining_arrows")
+	z_index = 10
 
 func _on_body_entered(body):
-    print("Colleded with: ", body.name)
-    if body.name == "Chicken": 
-        print("Game Over!")
-        get_tree().paused = true # Pause the game 
+	print("Collieded with: ", body.name)
+	if body.name == "AdultChickenChar": 
+		print("Game Over!") 
+		var gameworld = get_node_or_null("/root/GameWorld")
+		if gameworld:
+			gameworld.game_over()
 
-func _physics_process(delta):
-    position.y += speed * delta 
-    if position.y > 900: 
-        queue_free()
+func _physics_process(delta): 
+	position.y += speed * delta 
+	if position.y > 900: 
+		queue_free()
